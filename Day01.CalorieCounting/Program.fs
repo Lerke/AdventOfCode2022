2415ba27fbb9651699996889e734bafadefac22f
Willem <willemsporen@gmail.com>
2022-12-01 17:21:12 +0100

    Day 1 2 stars

---
+++ Day01.CalorieCounting/Program.fs
@@ -0,0 +1,38 @@
+﻿open System.IO
+
+let InputAccumulator (acc: string list list) curr =
+  match curr with
+  | "" -> List.append acc [[]]
+  | f -> match List.rev acc with
+         | x::xs -> List.append xs [(List.append x [f])]
+
+let CalculateCaloriesPerElf (input: string list) =
+  input
+  |> List.map(int)
+  |> List.sum
+  
+let ReadPuzzleInput file =
+  file
+  |> File.ReadAllLines
+  |> Array.fold InputAccumulator [[]]
+  |> List.map CalculateCaloriesPerElf
+
+[<EntryPoint>]
+let main argv =
+  printfn "* Advent of Code 2022 - Calorie Counting"
+
+  match Array.length argv with
+  | x when x = 1 ->
+    match File.Exists(argv[0]) with
+    | true ->
+      let totalCaloriesPerElf = ReadPuzzleInput argv[0] |> List.sortDescending
+      
+      printfn "Max calories for given elves: %i" (totalCaloriesPerElf |> List.head)
+      printfn "Total calories top three elves: %i" (totalCaloriesPerElf |> List.take 3 |> List.sum)
+      0
+    | _ ->
+      printfn "Did not find file!"
+      1
+  | _ ->
+    printfn "Usage: dotnet <path-to-input>"
+    1
--- readme.md
+++ readme.md
@@ -2,6 +2,6 @@
 
 My own solutions to the Advent of Code 2022 problems.
 
-| Day | Language | Progress      | Solution |
-| -- | -------- |---------------| -------- |
-|  1 | F#       | :star2: :star2: | [Day01.CalorieCounting](https://github.com/Lerke/AdventOfCode2022/tree/main/Day01.CalorieCounting)
+| Day | Language | Progress | Solution |
+| -- | -------- |--------------- | --------- |
+| 1 | F# | :star2: :star2: | [Day01.CalorieCounting](https://github.com/Lerke/AdventOfCode2022/tree/main/Day01.CalorieCounting)
