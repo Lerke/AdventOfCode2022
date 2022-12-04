3cca22aa8c599cfc3dc6887937ee2fc44c527d86
Willem <willemsporen@gmail.com>
2022-12-04 14:19:41 +0100

    Day 4 - 2 stars

--- Day04.CampCleanup/Program.fs
+++ Day04.CampCleanup/Program.fs
@@ -8,12 +8,28 @@ type ElfAssignment(line: string) =
   member this.FullyOverlaps(other: ElfAssignment) =
     List.forall (fun f -> (List.contains f other.Range)) this.Range
 
+  member this.PartialOverlap(other: ElfAssignment) =
+    List.exists (fun f -> (List.contains f other.Range)) this.Range
+
 type ElfPair(line: string) =
   member this.Assignments =
     line.Split [| ',' |]
     |> fun f ->
          [ (ElfAssignment f[0])
            (ElfAssignment f[1]) ]
+
+  member this.HasPartialOverlap =
+    match this.Assignments[0].Range.Length
+          <= (this.Assignments[1].Range.Length)
+      with
+    | true ->
+      this
+        .Assignments[ 0 ]
+        .PartialOverlap this.Assignments[1]
+    | false ->
+      this
+        .Assignments[ 1 ]
+        .PartialOverlap this.Assignments[0]
 
   member this.HasFullOverlap =
     match this.Assignments[0].Range.Length
@@ -36,9 +52,20 @@ let main argv =
   | x when x = 1 ->
     match File.Exists(argv[0]) with
     | true ->
-      let inputPairs = argv[0] |> File.ReadAllLines |> Array.map ElfPair
-      let overlappingPairs = inputPairs |> Array.filter (fun f -> f.HasFullOverlap)
+      let inputPairs =
+        argv[0] |> File.ReadAllLines |> Array.map ElfPair
+
+      let overlappingPairs =
+        inputPairs
+        |> Array.filter (fun f -> f.HasFullOverlap)
+
       printfn "[*] Number of overlapping pairs: %i" overlappingPairs.Length
+
+      let partialOverlappingPairs =
+        inputPairs
+        |> Array.filter (fun f -> f.HasPartialOverlap)
+
+      printfn "[**] Number of partial overlapping pairs: %i" partialOverlappingPairs.Length
       0
     | _ ->
       printfn "Did not find file!"
--- readme.md
+++ readme.md
@@ -7,3 +7,4 @@ My own solutions to the Advent of Code 2022 problems.
 | 1   | F#       | :star2: :star2: | [Day01.CalorieCounting](https://github.com/Lerke/AdventOfCode2022/tree/main/Day01.CalorieCounting)               |
 | 2   | F#       | :star2: :star2: | [Day02.RockPaperScissors](https://github.com/Lerke/AdventOfCode2022/tree/main/Day02.RockPaperScissors)           |
 | 3   | F#       | :star2: :star2: | [Day03.RucksackReorganization](https://github.com/Lerke/AdventOfCode2022/tree/main/Day03.RucksackReorganization) |
+| 4   | F#       | :star2: :star2: | [Day04.CampCleanup](https://github.com/Lerke/AdventOfCode2022/tree/main/Day04.CampCleanup)                       |
