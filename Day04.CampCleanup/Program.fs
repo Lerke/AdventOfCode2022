df3f061eb9c33b53dca4dcb62abd54cdefdf5347
Willem <willemsporen@gmail.com>
2022-12-04 14:15:55 +0100

    Day 4 - 1 star

---
+++ Day04.CampCleanup/Program.fs
@@ -0,0 +1,48 @@
+﻿open System.IO
+
+type ElfAssignment(line: string) =
+  member this.Range =
+    line.Split [| '-' |]
+    |> fun f -> [ (f[0] |> int) .. (f[1] |> int) ]
+
+  member this.FullyOverlaps(other: ElfAssignment) =
+    List.forall (fun f -> (List.contains f other.Range)) this.Range
+
+type ElfPair(line: string) =
+  member this.Assignments =
+    line.Split [| ',' |]
+    |> fun f ->
+         [ (ElfAssignment f[0])
+           (ElfAssignment f[1]) ]
+
+  member this.HasFullOverlap =
+    match this.Assignments[0].Range.Length
+          <= (this.Assignments[1].Range.Length)
+      with
+    | true ->
+      this
+        .Assignments[ 0 ]
+        .FullyOverlaps this.Assignments[1]
+    | false ->
+      this
+        .Assignments[ 1 ]
+        .FullyOverlaps this.Assignments[0]
+
+[<EntryPoint>]
+let main argv =
+  printfn "* Advent of Code 2022 - Camp Cleanup"
+
+  match Array.length argv with
+  | x when x = 1 ->
+    match File.Exists(argv[0]) with
+    | true ->
+      let inputPairs = argv[0] |> File.ReadAllLines |> Array.map ElfPair
+      let overlappingPairs = inputPairs |> Array.filter (fun f -> f.HasFullOverlap)
+      printfn "[*] Number of overlapping pairs: %i" overlappingPairs.Length
+      0
+    | _ ->
+      printfn "Did not find file!"
+      1
+  | _ ->
+    printfn "Usage: dotnet <path-to-input>"
+    1
