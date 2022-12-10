d3e95e57492663e6ee98be72c31a3152b9ceca34
Willem <willemsporen@gmail.com>
2022-12-10 02:12:18 +0100

    Day 8 - One star

--- Day08.TreetopTreeHouse/Program.fs
+++ Day08.TreetopTreeHouse/Program.fs
@@ -1,4 +1,53 @@
-﻿open System.IO
+﻿open System
+open System.IO
+
+type Position(x: int, y: int) =
+    member this.x = x
+    member this.y = y
+    static member Left = Position(-1, 0)
+    static member Right = Position(1, 0)
+    static member Up = Position(0, 1)
+    static member Down = Position(0, -1)
+    static member (+)(a: Position, b: Position) = Position(a.x + b.x, a.y + b.y)
+
+type Tree(position: Position, height: int) =
+    member val position = position
+    member val height = height
+
+    member this.VisibleFromDirection direction (map: TreeMap) h =
+        let np = this.position + direction
+
+        match np with
+        | p when p.x < 0 || p.y < 0 -> true
+        | p when p.x > ((Array.length map.Map) - 1) || p.y > ((Array.length map.Map) - 1) -> true
+        | _ ->
+            match map.AtPosition(np) with
+            | q when q.height < h -> q.VisibleFromDirection direction map h
+            | _ -> false
+
+    member this.DirectionsVisibleFrom map =
+        [ Position.Left; Position.Up; Position.Down; Position.Right ]
+        |> List.map (fun f -> this.VisibleFromDirection f map height)
+        |> List.filter (fun f -> f = true)
+        |> List.length
+
+and TreeMap(map: Tree [] []) =
+    member val Map = map
+
+    member this.AtPosition(position: Position) : Tree = this.Map.[position.y].[position.x]
+
+    member this.Print() =
+        this.Map
+        |> Array.iter (fun f -> printfn "%s" ((f |> (Array.fold (fun acc curr -> acc + (curr.height |> string)) ""))))
+
+    member this.VisibilityMap =
+        this.Map
+        |> Array.map (fun f -> f |> Array.map (fun q -> (q.DirectionsVisibleFrom this)))
+
+    member this.PrintVisibilityMap() =
+        this.VisibilityMap
+        |> Array.iter (fun f -> printfn "%s" ((f |> (Array.fold (fun acc curr -> acc + (curr |> string)) ""))))
+
 
 [<EntryPoint>]
 let main argv =
@@ -8,6 +57,30 @@ let main argv =
     | x when x = 1 ->
         match File.Exists(argv[0]) with
         | true ->
+            let trees =
+                TreeMap(
+                    (File.ReadAllLines argv[0])
+                    |> Array.mapi (fun y f ->
+                        (f
+                         |> seq
+                         |> Seq.mapi (fun x q -> Tree(Position(x, y), (Convert.ToInt32(q |> string))))
+                         |> Seq.toArray))
+                )
+
+            printfn ""
+            trees.Print()
+            printfn ""
+            trees.PrintVisibilityMap()
+            printfn ""
+
+            let visibleTrees =
+                trees.VisibilityMap
+                |> (Array.collect id)
+                |> (Array.filter (fun f -> f > 0))
+                |> Array.length
+
+            printfn "[* ] Number of trees visible: %i" visibleTrees
+
             0
         | _ ->
             printfn "Did not find file!"
--- readme.md
+++ readme.md
@@ -11,3 +11,4 @@ My own solutions to the Advent of Code 2022 problems.
 | 5   | F#       | :star2: :star2: | [Day05.SupplyStacks](https://github.com/Lerke/AdventOfCode2022/tree/main/Day05.SupplyStacks)                     |
 | 6   | F#       | :star2: :star2: | [Day06.TuningTrouble](https://github.com/Lerke/AdventOfCode2022/tree/main/Day06.TuningTrouble)                   |
 | 7   | F#       | :star2: :star2: | [Day07.NoSpaceLeftOnDevice](https://github.com/Lerke/AdventOfCode2022/tree/main/Day07.NoSpaceLeftOnDevice)       |
+| 8   | F#       | :star2:         | [Day08.TreetopTreeHouse](https://github.com/Lerke/AdventOfCode2022/tree/main/Day08.TreetopTreeHouse)             |
