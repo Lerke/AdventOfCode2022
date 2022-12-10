a64b861687879f5dc27ef16f7f94b73592f83bca
Willem <willemsporen@gmail.com>
2022-12-10 02:41:14 +0100

    Day 8 - Two stars

--- Day08.TreetopTreeHouse/Program.fs
+++ Day08.TreetopTreeHouse/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 open System.IO
+open Microsoft.FSharp.Control.TaskBuilderExtensions
 
 type Position(x: int, y: int) =
     member this.x = x
@@ -14,22 +15,27 @@ type Tree(position: Position, height: int) =
     member val position = position
     member val height = height
 
-    member this.VisibleFromDirection direction (map: TreeMap) h =
+    member this.VisibleFromDirection direction (map: TreeMap) h score =
         let np = this.position + direction
 
         match np with
-        | p when p.x < 0 || p.y < 0 -> true
-        | p when p.x > ((Array.length map.Map) - 1) || p.y > ((Array.length map.Map) - 1) -> true
+        | p when p.x < 0 || p.y < 0 -> (true, score)
+        | p when p.x > ((Array.length map.Map) - 1) || p.y > ((Array.length map.Map) - 1) -> (true, score)
         | _ ->
             match map.AtPosition(np) with
-            | q when q.height < h -> q.VisibleFromDirection direction map h
-            | _ -> false
+            | q when q.height < h -> q.VisibleFromDirection direction map h (score + 1)
+            | _ -> (false, score + 1)
 
     member this.DirectionsVisibleFrom map =
         [ Position.Left; Position.Up; Position.Down; Position.Right ]
-        |> List.map (fun f -> this.VisibleFromDirection f map height)
-        |> List.filter (fun f -> f = true)
+        |> List.map (fun f -> this.VisibleFromDirection f map height 0)
+        |> List.filter (fun f -> (fst f) = true)
         |> List.length
+
+    member this.VisibilityScore map =
+        [ Position.Left; Position.Up; Position.Down; Position.Right ]
+        |> List.map (fun f -> this.VisibleFromDirection f map height 0)
+        |> ((List.fold (fun acc curr -> acc * (snd curr)) 1))
 
 and TreeMap(map: Tree [] []) =
     member val Map = map
@@ -44,8 +50,20 @@ and TreeMap(map: Tree [] []) =
         this.Map
         |> Array.map (fun f -> f |> Array.map (fun q -> (q.DirectionsVisibleFrom this)))
 
+    member this.ScenicScoreMap =
+        this.Map
+        |> Array.map (fun f -> f |> Array.map (fun q -> (q.VisibilityScore this)))
+
     member this.PrintVisibilityMap() =
+        printfn "Visibility view"
+
         this.VisibilityMap
+        |> Array.iter (fun f -> printfn "%s" ((f |> (Array.fold (fun acc curr -> acc + (curr |> string)) ""))))
+
+    member this.PrintScenicScoreMap() =
+        printfn "Scenic view"
+
+        this.ScenicScoreMap
         |> Array.iter (fun f -> printfn "%s" ((f |> (Array.fold (fun acc curr -> acc + (curr |> string)) ""))))
 
 
@@ -72,6 +90,8 @@ let main argv =
             printfn ""
             trees.PrintVisibilityMap()
             printfn ""
+            trees.PrintScenicScoreMap()
+            printfn ""
 
             let visibleTrees =
                 trees.VisibilityMap
@@ -80,6 +100,13 @@ let main argv =
                 |> Array.length
 
             printfn "[* ] Number of trees visible: %i" visibleTrees
+            
+            let highestScenicScore =
+                trees.ScenicScoreMap
+                |> (Array.collect id)
+                |> Array.max
+            
+            printfn "[**] Highest scenic score possible for tree in this map: %i" highestScenicScore
 
             0
         | _ ->
