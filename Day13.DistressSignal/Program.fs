1486ee610c827c5508a02d048e058082af8cf5c7
Willem <willemsporen@gmail.com>
2022-12-28 11:59:39 +0100

    Day 13 - Basic parsing

--- Day13.DistressSignal/Program.fs
+++ Day13.DistressSignal/Program.fs
@@ -1,30 +1,63 @@
-﻿
-type PacketContent =
-    | Number of int
-    | NumberList of PacketContent list
+﻿open Microsoft.FSharp.Core
 
-let rec ParseLine (line: string list) =
-    match line with
-    | x::xs ->
-        match x with
-        | "[" -> NumberList [(ParseLine xs)]
-        | _ -> Number 1
-    | [] -> Number 1
+type Token =
+  | NumberToken of string
+  | ListStartToken
+  | ListEndToken
+  | SeparatorToken
 
+type ParsedToken =
+  | Number of int
+  | NumberList of ParsedToken list
+
+let rec ParseLine (line: char list) (context: Token list) =
+  match line with
+  | x :: xs ->
+    match x with
+    | '[' -> ParseLine xs (ListStartToken :: context)
+    | ']' -> ParseLine xs (ListEndToken :: context)
+    | ',' -> ParseLine xs (SeparatorToken :: context)
+    | x ->
+      ParseLine
+        xs
+        ((match context.Head with
+          | NumberToken f -> NumberToken(f + (x |> string))
+          | _ -> NumberToken(x |> string))
+         :: context)
+  | [] -> context |> List.filter (fun f -> f <> SeparatorToken) |> List.rev
+
+let rec ParseTokens (stack: Token list) =
+  let rec parseStack (stack: Token list) =
+    let mutable arr = []
+    let mutable s = stack
+
+    while ((not s.IsEmpty) && s.Head <> ListEndToken) do
+      match s.Head with
+      | NumberToken n ->
+        arr <- List.append arr [ Number(n |> int) ]
+        s <- s.Tail
+      | ListStartToken ->
+        let (r, stack: Token list) = parseStack (s.Tail)
+        arr <- List.append [ (NumberList r) ] arr
+        s <- stack.Tail
+
+    (arr, s)
+
+  (fst (parseStack stack))[0]
 
 [<EntryPoint>]
 let main argv =
-    printfn "* Advent of Code 2022 - Hill Climbing Algorithm"
+  printfn "* Advent of Code 2022 - Distress Signal"
 
-    match Array.length argv with
-    | x when x = 1 ->
-        let line = "[1,1,3,1,1]"
-        let parsedLine = ParseLine (line.Split "" |> Array.toList)
-        0
-    | _ ->
-        printfn "Did not find file!"
-        let a = NumberList [Number 1; Number 2]
-        1
-    | _ ->
-        printfn "Usage: dotnet <path-to-input>"
-        1
+  match Array.length argv with
+  | x when x = 1 ->
+    // let line = "[1,1,3,1,1]"
+    // let parsedLine = ParseLine (line.ToCharArray() |> Array.toList) []
+    let parsedTwo = ParseLine ("[[4,4],4,4]".ToCharArray() |> Array.toList) []
+
+    // let q = ParseTokens parsedLine
+    let v = ParseTokens parsedTwo
+    0
+  | _ ->
+    printfn "Did not find file!"
+    1
